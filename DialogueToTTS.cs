33a7e7dc90c44b037abcdef1ea461041b7a5332c
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2022-03-02 10:42:51 +0100

    Added vocoder_name & model_name parameters, Added Initial Verbose/Log messages, Cleaning a bit the code.

--- DialogueToTTS.cs
+++ DialogueToTTS.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Data;
-using System.Diagnostics;
-using System.Globalization;
+using DialogueToTTS.DTO; // for ArrowDialogueParserFormat
+using System.Collections; // for IEnumerable
+using System.Linq; // for ToList() 
+using System.Diagnostics; // for ProcessStartInfo
 using System.IO;
-using System.Linq;
-using CsvHelper;
-using DialogueToTTS.DTO;
+
 
 namespace DialogueToTTS
 {
@@ -17,59 +15,107 @@ namespace DialogueToTTS
         public DialogueToTTS(string filename, string path = null)
         {
             this.filename = filename;
-            if(path == null)
+            if (path == null)
             {
                 path = Directory.GetCurrentDirectory() + "/output/";
-            }else{this.path = path ;}
-            
+            }
+            else { this.path = path; }
+
         }
 
         public bool Run()
         {
+            log("Starting Process");
             ProcessStartInfo temp;
             var rows = GetDataTable(filename);
-            if(rows != null)
-                foreach(ArrowDialogueParserFormat row in rows)
+            if (rows != null)
+            {
+                foreach (ArrowDialogueParserFormat row in rows)
                 {
-                    if (row.DialogueWaveId.Contains("#")){
+                    if (row.DialogueWaveId.Contains("#"))
+                    {
                         var id = row.DialogueWaveId.Split("#");
                         if (!Directory.Exists(path + "/" + id[0]))
                         {
                             Directory.CreateDirectory(path + "/" + id[0]);
                         }
-                        
+
                         temp = new ProcessStartInfo()
                         {
                             FileName = "tts",
                             Arguments =
                                 "--text \"" + row.Text + "\" " +
-                                "--out_path "+ path + "/" + id[0]+"/"+id[1]+".wav "
-                                //+"--use-cuda "+"true",
-                            
+                                chooseModel(row.Voice) + chooseVocoder(row.Voice) +
+                                "--out_path " + path + "/" + id[0] + "/" + id[1] + ".wav "
+                            //+"--use-cuda "+"true",
+
                         };
                         Process.Start(temp).WaitForExit();
                     }
                 }
+            }
+            else
+            {
+                log("No Rows Found");
+            }
             return true;
         }
 
-        IEnumerable GetDataTable(string filepath)
+        /// TODO: JSON Config load settings
+        private string chooseModel(string voice)
         {
+            string tmp = " --model_name ";
+            switch (voice)
+            {
+                case "Player": tmp += "tts_models/en/ek1/tacotron2"; break;
+                case "Radio": tmp += "tts_models/en/ljspeech/vits"; break;
+                case "Auntie Robot": // Default Is Fine
+                default: tmp = ""; break;
+            }
+            return tmp + " ";
+        }
+
+        /// TODO: JSON Config load settings
+        private string chooseVocoder(string voice)
+        {
+            string tmp = "--vocoder_name ";
+            switch (voice)
+            {
+                case "Player": tmp += "vocoder_models/en/ek1/wavegrad"; break;
+                case "Radio": tmp += "vocoder_models/en/ljspeech/univnet"; break;
+                case "Auntie Robot": // DEfault is Fine
+                default: tmp = ""; break;
+            }
+            return tmp + " ";
+        }
+
+        private IEnumerable GetDataTable(string filepath)
+        {
+            log("Reading Datatable from: " + filepath);
             try
             {
                 using (var reader = new StreamReader(filepath))
-                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-                    {
-                    return csv.GetRecords<ArrowDialogueParserFormat>().ToList();;
-                    }
+                using (var csv = new CsvHelper.CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    return csv.GetRecords<ArrowDialogueParserFormat>().ToList(); ;
+                }
             }
-            catch
+            catch (DirectoryNotFoundException)
             {
-                return null;
+                log("Directory not found");
             }
-            
+            catch (System.Exception ex)
+            {
+                log("Generic Exception:" + ex.ToString());
+            }
+            return null;
         }
 
-        
+        // TODO: make a logger
+        private void log(string log, [System.Runtime.CompilerServices.CallerMemberName] string functionName = "")
+        {
+            System.Console.WriteLine("LOG: [" + functionName + "]: " + log);
+        }
+
     }
 }
